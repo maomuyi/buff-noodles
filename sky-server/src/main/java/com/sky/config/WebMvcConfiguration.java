f9aad0dcf5e58f708d704b97f372c5d858ae5c5b
maomu <2048683154@qq.com>
2024-07-06 23:44:11 +0800

    新增员工（已通过前后端测试）

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -43,10 +43,11 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
      */
     @Bean
     public Docket docket() {
+        log.info("准备生成接口文档。。。");
         ApiInfo apiInfo = new ApiInfoBuilder()
-                .title("苍穹外卖项目接口文档")
+                .title("卯木宅急送项目接口文档")
                 .version("2.0")
-                .description("苍穹外卖项目接口文档")
+                .description("卯木宅急送项目接口文档")
                 .build();
         Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo)
