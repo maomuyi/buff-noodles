b395ab73cc2d431a132fe9ce92e4e86c75b3f46e
maomu <2048683154@qq.com>
2024-07-12 03:00:33 +0800

    菜品删除

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -12,4 +13,17 @@ public interface DishFlavorMapper {
      * @param flavors
      */
     void insertBath(List<DishFlavor> flavors);
+
+    /**
+     * 根据菉品id劂除对应的囗味数据
+     * @param dishId
+     */
+    @Delete("delete from dish_flavor where dish_id = #{dishId}")
+    void deleteById(Long dishId);
+
+    /**
+     * 根据菜品id集合批量删除关联的口味数据
+     * @param dishIds
+     */
+    void deleteByIds(List<Long> dishIds);
 }
